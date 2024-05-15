ebcb2c0666004f3b45b570f0bd10a33455459906
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 01:17:33 +0500

    This is 45 Question Assignment Q-26.
    This is 45 Question Assignment Q-26.

---
+++ app.js
@@ -0,0 +1,16 @@
+var alienColor = "green";
+// If block version
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+// Else block version
+alienColor = "red"; // Change the color for testing
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("You just earned 10 points.");
+}
---
+++ app.ts
@@ -0,0 +1,17 @@
+let alienColor: string = "green";
+
+// If block version
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien.");
+} else {
+    console.log("You just earned 10 points.");
+}
+
+// Else block version
+alienColor = "red"; // Change the color for testing
+
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the alien.");
+} else {
+    console.log("You just earned 10 points.");
+}
